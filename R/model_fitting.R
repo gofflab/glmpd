fba4f59bfdff3d5db572dcc6009edd5872313fb4
Loyal Goff <loyalgoff@gmail.com>
2020-12-16 10:24:02 -0500

    Reorg / Refactor after Alina's code dump. data moved to /inst/extdata. Functionalized code from alina/coeff_table_glmpd.R and getProjectionDrivers.R moved to 'R/model_fitting.R'. alina/testScript.R used as basis for 'vignettes/test.Rmd'

--- R/model_fitting.R
+++ R/model_fitting.R
@@ -17,7 +17,7 @@
 #' @import MASS
 #' @export
 #'
-#' @examples
+# #' @examples
 fit_model_cds<-function(cds,pattern,cores,exp_family="negbinomial",clean_model=F,verbose=T){
   #TODO: Add 'pattern' to cds unless already there
 
@@ -31,3 +31,137 @@ fit_model_cds<-function(cds,pattern,cores,exp_family="negbinomial",clean_model=F
                                        clean_model = clean_model, verbose = verbose)
   return(glm_models)
   }
+
+# From Alina's code dump
+
+#' fit_helper
+#'
+#' @param thisGene String of gene name
+#' @param thisPattern String of pattern name
+#' @param model_formula_string Model Formula String
+#' @param annotDF Not sure what this argument is (alina?)
+#'
+#' @return
+#' @importFrom stats as.formula
+#' @import MASS
+#' @import dplyr
+#' @export
+#'
+# #' @examples
+fit_helper <- function(thisGene, thisPattern, model_formula_string, annotDF){
+
+  # sample x annotations, gene expression, and p weights
+  datFrame <- as.data.frame(merge(annotDF,thisGene,by=0,sort=FALSE))
+  datFrame$patternWeights <- thisPattern
+
+  # prepare model formula string
+  model_formula_str <- paste("y", model_formula_string, sep = "")
+  model_formula = stats::as.formula(model_formula_str)
+
+  # run model
+  tryCatch({
+    FM_fit <- glm.nb(model_formula,
+                     data = datFrame, link = log,
+                     control = glm.control(maxit = 50))
+    FM_summary = summary(FM_fit)
+    df = list(model = FM_fit, model_summary = FM_summary)
+  }, error = function(e){
+    return(conditionMessage(e)) # return error messages; don't stop iterating
+  })
+}
+
+
+# countsMatrix: genes x samples matrix with gene counts
+# annotDF: samples x annotations matrix. includes condition(s) of interest
+# projectedPs: patterns x sample matrix with projected p weights (learned with projectR)
+# returns a list of model fits for each pattern. Each item in that list contains a fit
+#      for each gene
+#' Title
+#'
+#' @param countsMatrix genes x samples matrix with gene counts
+#' @param annotDF samples x annotations matrix. includes condition(s) of interest
+#' @param projectedPs patterns x sample matrix with projected p weights (learned with projectR)
+#' @param model_formula_str
+#'
+#' @return A list of model fits for each pattern. Each item in that list contains a fit for each gene
+#' @export
+#'
+# #' @examples
+getProjectionDrivers <- function(countsMatrix, annotDF, projectedPs, model_formula_str){
+
+  # ensure that all genes in countsMatrix are expressed in at least one sample
+  totalCounts <- apply(countsMatrix,1,sum)
+  numZeroExpGenes <- sum(totalCounts==0)
+  if(numZeroExpGenes>0)
+    stop(paste("Number of genes with 0 counts across all samples:",numZeroExpGenes))
+
+  # fit models for each pattern
+  fits <- list()
+  for (pattI in 1:(dim(projectedPs)[1])){
+    print(paste0("running models for projection driver analysis: pattern ", pattI, " of ",dim(projectedPs)[1]))
+    thisPatt <- projectedPs[pattI,]
+    # fit a model for each gene
+    fits[[pattI]] <- apply(countsMatrix,1,fit_helper,thisPattern=thisPatt,model_formula_string=model_formula_str,annotDF=annotDF)
+  }
+  names(fits) <- rownames(projectedPs)
+  return(fits)
+}
+
+#' extractCoeffHelper_glm_nb
+#'
+#' @param model A fitted GLM model
+#' @param model_summary Not sure what this argument is offhand (Alina?)
+#'
+#' @return
+#' @import tibble
+#' @export
+#'
+# #' @examples
+extractCoeffHelper_glm_nb <- function (model, model_summary)
+{
+  coef_mat <- model_summary[["coefficients"]]
+  coef_mat <- apply(coef_mat, 2, function(x) {
+    as.numeric(as.character(x))
+  })
+  row.names(coef_mat) = row.names(model_summary[["coefficients"]])
+  colnames(coef_mat) = c("estimate", "std_err", "test_val",
+                         "p_value")
+  coef_mat = tibble::as_tibble(coef_mat, rownames = "term")
+  coef_mat$model_component = "count"
+  return(coef_mat)
+}
+
+
+#' coeff_table_glmpd
+#'
+#' @param fits A List of model fits. output of getProjectionDrivers.R
+#'
+#' @return A list of coefficient tables
+#' @import dplyr
+#' @import tibble
+#' @importFrom stats p.adjust
+#' @import purrr
+#' @export
+#'
+# #' @examples
+coeff_table_glmpd <- function(fits){
+
+  coeffTables <- list()
+  # one coefficient table per pattern
+  for (pattFitI in (1:length(fits)))
+  {
+    models <- fits[[pattFitI]]
+
+    successfulModels <- models[unlist(lapply(models,length)) > 1]
+    fm <- tibble::as_tibble(purrr::transpose(successfulModels))
+    fm_named = fm %>% dplyr::mutate(id = names(successfulModels))
+    M_f = fm_named %>% dplyr::mutate(terms = purrr::map2(.f = purrr::possibly(extractCoeffHelper_glm_nb,NA_real_), .x=model, .y= model_summary)) %>% tidyr::unnest(terms)
+    fit_coefs = M_f %>% dplyr::group_by(model_component, term) %>%
+      dplyr::mutate(q_value = stats::p.adjust(p_value)) %>%
+      dplyr::ungroup()
+
+    coeffTables[[pattFitI]] <- fit_coefs
+  }
+  names(coeffTables) <- names(fits)
+  return(coeffTables)
+}
